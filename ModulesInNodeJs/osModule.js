b39af93947a22f36bddf12b88424f95fff73125b
mjbharathgithub <mjbharath2003@gmail.com>
2024-06-03 19:49:14 +0530

    uses of os module

---
+++ ModulesInNodeJs/osModule.js
@@ -0,0 +1,18 @@
+const os= require("os");
+
+console.log(os.arch()); // checks the architecture whether 64 or 32 bit
+
+console.log(os.platform());
+
+
+console.log(os.cpus());
+
+
+console.log(os.totalmem());
+
+
+console.log(os.freemem());
+
+
+
+
