37d79a454ab4ca6291fc0b5dc62f6fe4f2fa7080
mjbharathgithub <mjbharath2003@gmail.com>
2024-06-03 20:30:36 +0530

    What is path and File Modules with basic crud operation of files

---
+++ ModulesInNodeJs/fileModule.js
@@ -0,0 +1,17 @@
+const fs= require("fs");
+
+console.log(fs.readFileSync("text.txt"));
+// note :<Buffer 54 68 69 73 20 69 73 20 61 20 73 61 6d 70 6c 65 20 66 69 6c 65 20 75 73 65 64 20 74 6f 20 6c 65 61 72 6e 20 66 69 6c 65 73 20 6d 6f 64 75 6c 65 20 69 ... 8 more bytes>  will be the output 
+// since in default it returns a buffer
+
+console.log(""+fs.readFileSync("text.txt"));
+// This is a sample file used to learn files module in nodejs
+//now we get the above output since its casted to string type
+
+
+
+fs.appendFileSync("text.txt","\nThis is appended using appendFileSync() method and this file too creates a new file if its not present ");
+
+fs.unlinkSync("new.txt");//this method deletes the content in the file
+
+fs.writeFileSync("new.txt","This method creates a new file and writes the message in the file if the file is already created then it overides the data in the file ");
---
+++ ModulesInNodeJs/pathModule.js
@@ -0,0 +1,7 @@
+const path=require("path");
+
+//note its best to use \\ instead of \ in nodejs while handling file paths
+
+console.log(path.extname("C:\\Users\\mjbha\\OneDrive\\Desktop\\Web dev\\webdevelopment\\BACK_END\\NODE\\ModulesInNodeJs\\pathModule.js"));
+
+console.log(path.basename("C:\\Users\\mjbha\\OneDrive\\Desktop\\Web dev\\webdevelopment\\BACK_END\\NODE\\ModulesInNodeJs\\pathModule.js"));
