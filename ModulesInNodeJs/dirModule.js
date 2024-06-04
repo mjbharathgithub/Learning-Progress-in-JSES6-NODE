ff570cf1f6f4410d6c196ec636a6dceb72cd23d6
mjbharathgithub <mjbharath2003@gmail.com>
2024-06-04 13:52:05 +0530

    what is direcory modules and some implementations

---
+++ ModulesInNodeJs/dirModule.js
@@ -0,0 +1,14 @@
+const fs=require("fs");
+
+//used to create a directory
+fs.mkdirSync("sampleDirectory");
+
+
+//used to find the files inside the directory
+console.log(fs.readdirSync("C:\\Users\\mjbha\\OneDrive\\Desktop\\Web dev\\webdevelopment\\BACK_END\\NODE\\ModulesInNodeJs\\sampleDirectory"));
+
+//check if the directory exists
+console.log(fs.existsSync("sampleDirectory"));
+
+//used to remove a directory
+fs.rmdirSync("sampleDirectory");
