fb9d04669a02aa6d0b0a10b745c6ff2fad54a585
Joseph Bharath <112538236+mjbharathgithub@users.noreply.github.com>
2024-10-29 13:11:55 +0530

    Update TestEE.js

--- 2.0 Creating And Loading Module/TestEE.js
+++ 2.0 Creating And Loading Module/TestEE.js
@@ -22,6 +22,36 @@ test.message("hello nodejs How are you");
 test.greeting("Happy morning");
 test.message("I am fine joseph");
 
+// Tody 
+const EventEmitter= require('events');
+
+class Messager extends EventEmitter{
+    message(user,data){
+        this.emit('message',{user,data});
+        
+    }
+}
+
+function writeMessage({user,data}){
+    console.log(`${user} Sending...\nmessage : ${data}
+    `);
+}
+
+const chatApp= new Messager();
+
+chatApp.on('message',({user,data})=>writeMessage({user,data}));
+//chatApp.on('videoCall',makeVideoCall({send,reciever}));
+
+chatApp.message('Joseph','Hi Events How are you');
+
+//chatApp.emit('videoCall',{send:'Joseph',rec:'Joi'});
+
+chatApp.message('Events','We are fine joseph');
+
+chatApp.removeListener('message',writeMessage);
+
+chatApp.message('Joseph','Oh thats great!');
+
 // on(event, listener): Adds a listener for an event.
 // once(event, listener): Adds a one-time listener for an event.
 // emit(event, [args]): Triggers an event, optionally passing arguments.
